1f10dfe4d5c10ca19f3b86eacd591f2de0196f18
Chris <tacashix@gmail.com>
2021-01-26 15:41:34 +0100

    testing more

--- funcs.go
+++ funcs.go
@@ -214,15 +214,15 @@ func sendPayload(conf Config, payload string) error {
 	if err != nil {
 		return err
 	}
-	//conn.SetDeadline(time.Now().Add(time.Duration(conf.Timeout) * time.Second))
+	conn.SetDeadline(time.Now().Add(time.Duration(conf.Timeout) * time.Second))
 	defer conn.Close()
-	/*
-		reply := make([]byte, 1024)
-		_, err = conn.Read(reply)
-		if err != nil {
-			return err
-		}
-	*/
+
+	reply := make([]byte, 1024)
+	_, err = conn.Read(reply)
+	if err != nil {
+		return err
+	}
+
 	_, err = fmt.Fprintf(conn, payload+"\r\n")
 	if err != nil {
 		return err
