4ea7e247929d978f2aa7dea8fed0d14e1ea86cbd
Chris <tacashix@gmail.com>
2021-01-26 14:49:54 +0100

    more timeout stuff

--- funcs.go
+++ funcs.go
@@ -211,12 +211,11 @@ func execute(c Config, e Exploit) {
 
 func sendPayload(conf Config, payload string) error {
 	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", conf.Host, conf.Port), time.Duration(conf.Timeout)*time.Second)
-	//conn.SetDeadline(time.Now().Add(time.Duration(conf.Timeout) * time.Second))
-	defer conn.Close()
 	if err != nil {
 		return err
 	}
-
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(time.Duration(conf.Timeout) * time.Second))
 	reply := make([]byte, 1024)
 	_, err = conn.Read(reply)
 	if err != nil {
